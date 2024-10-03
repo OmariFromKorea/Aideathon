df5b8222724c52d536547a5684fe7f6826cb1139
OmariFromKorea <162781745+OmariFromKorea@users.noreply.github.com>
2024-10-03 19:10:45 +0900

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,5 +1,7 @@
+#탈출구까지 최단경로 계산 기술적 구현(예시)
+
 import heapq  
-def dijkstra(graph, start): 
+def dijkstra(graph, start): #다익스트라 최단경로 함수
     distances = {node: float('inf') for node in graph}
     distances[start] = 0
     previous_nodes = {node: None for node in graph}
@@ -26,6 +28,7 @@ def get_shortest_path(previous_nodes, start, target):  # 최단경로 탐색 함
         current_node = previous_nodes[current_node]
     path.reverse()  
     return path
+    
 # 가중치가 있는 무방향 그래프
 graph = {
     'gate1': {'gate3': 1, 'gate4': 3, 'gate5': 6},
@@ -35,10 +38,12 @@ graph = {
     'gate5': {'gate1': 6, 'gate3': 2, 'gate4': 5, 'exit': 2},
     'exit' : {'gate5': 2}
 }
-# gate1에서 최단경로 계산
+
+# gate1에서 exit 최단경로 계산
 start_node = 'gate1'
 target_node = 'exit'
 shortest_paths, previous_nodes = dijkstra(graph, start_node) 
+
 # gate1에서 exit까지 최단 경로 출력
 path = get_shortest_path(previous_nodes, start_node, target_node)
 print(f"최단 경로 from {start_node} to {target_node}: {path} with distance {shortest_paths[target_node]}")
